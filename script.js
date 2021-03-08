6b5a4f58e4ba03ee9fad209575d2c55ed40bd1fa
Dominic de Bettencourt <dominicd@berkeley.edu>
2021-03-08 15:21:34 -0800

    resume

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ function toggleText() {
     text.classList.toggle("show");
     text.classList.toggle("hide");
   }
+let text = document.getElementById("resume");
+text.onclick = changeText();
+
+function changeText() {
+  let text = document.getElementById("resume");
+  if (text.innerHTML === "Resume"){
+    text.innerHTML = "Pause";
+  } else {
+    text.innerHTML = "Resume";
+  }
+}
